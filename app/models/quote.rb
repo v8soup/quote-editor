6f7a14e47295a4409fa62aa54aeb545423d17235
Ramona <v8soup@users.noreply.github.com>
2025-01-03 20:21:19 -0800

    Use async Turbo

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,10 @@ class Quote < ApplicationRecord
     validates :name, presence: true
 
     scope :ordered, -> { order(created_at: :desc)}
+
+    # after_create_commit { broadcast_prepend_later_to "quotes" }
+    # after_update_commit { broadcast_replace_later_to "quotes" }
+    # after_destroy_commit { broadcast_remove_to "quotes" }
+    # Above is equivalent to below
+    broadcasts_to -> (quote) { "quotes" }, inserts_by: :prepend
 end
